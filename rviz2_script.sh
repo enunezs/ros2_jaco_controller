39b88f404d559c9925d980975197304738e52152
Emanuel Nunez <github@enunezs.com>
2022-10-29 16:29:37 +0100

    Almost there for grabber

--- rviz2_script.sh
+++ rviz2_script.sh
@@ -1,18 +1,18 @@
 xhost +local:root
 
-docker image build -t ros2-panda .     
 
 docker run -it \
 	--env="DISPLAY" \
-	--device=/dev/video0:/dev/video0 \
-	-e DISPLAY=$DISPLAY \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--privileged \
-	-e "ROS_DOMAIN_ID=7" \
 	--net=host \
+	-e "ROS_DOMAIN_ID=7" \
 	-v /dev/shm:/dev/shm \
-	-u 0 ros2-panda
+	osrf/ros:foxy-desktop \
+	rviz2
 
 export containerId=$(docker ps -l -q)
+
+
 
