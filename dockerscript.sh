453e3aed97b2f7fb917f945a035ba35955be44fe
Emanuel Nunez <github@enunezs.com>
2023-09-04 17:02:10 +0100

    Fixed installation with gui

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,7 +1,17 @@
 xhost +local:root
 
+<<<<<<< .merge_file_u7NzRR
 docker image build -t kinova-ros2 --build-arg DISPLAY=:0 --network=host .    
+=======
+<<<<<<< HEAD
+docker image build -t ros2-panda .    
  
+
+=======
+docker image build -t kinova-ros2 .    
+>>>>>>> .merge_file_dovT0W
+ 
+>>>>>>> 32b90d2f783f4f6db2785f3dcba0953584cc9a6d
 docker run -it \
 	--env="DISPLAY" --device=/dev/video0:/dev/video0 \
 	-e DISPLAY=$DISPLAY --env="QT_X11_NO_MITSHM=1" \
