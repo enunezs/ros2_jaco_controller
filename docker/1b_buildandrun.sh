33ff82e41a9c3aec8d87a4d610ec481e351ced05
Emanuel Nunez <github@enunezs.com>
2024-03-14 21:32:26 +0000

    Docker organised

---
+++ docker/1b_buildandrun.sh
@@ -0,0 +1,18 @@
+xhost +local:root
+
+docker image build -t kinova-ros2:latest -f docker/Dockerfile docker     
+
+docker run -it --env="DISPLAY" \
+	--device=/dev/video0:/dev/video0 \
+	--env DISPLAY=$DISPLAY \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--privileged \
+	--env "ROS_DOMAIN_ID=7" \
+	--net=host \
+	--volume $(pwd):/root/ws/origami \
+	--volume /dev/shm:/dev/shm \
+	kinova-ros2:latest
+
+export containerId=$(docker ps -l -q)
+
---
+++ docker/Ubuntu/64bits/installSDK64.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo 'KINOVA SDK INSTALLATION'
+echo 'This operation require root privilege.'
+
+sudo dpkg -i KinovaAPI-5.2.0-amd64.deb
+./jaco2Install64_1.0.0
+
+
+
+
