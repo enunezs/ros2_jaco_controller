8afec6e1472a0b122eae57c1cf33e72e4e592e27
Emanuel Nunez <github@enunezs.com>
2024-03-14 21:34:36 +0000

    re-organised repo as a package

---
+++ launch/jaco_and_controller.launch.py
@@ -0,0 +1,48 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import ThisLaunchFileDir, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    launch_description = LaunchDescription()
+
+    # Robot connection
+    kinova_robot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("kinova_bringup"), "launch"),
+                "/kinova_robot_launch.py",
+            ]
+        )
+    )
+    launch_description.add_action(kinova_robot_launch)
+
+    # Moveit2
+    moveit_robot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("kinova_bringup"), "launch"),
+                "/moveit_robot_launch.py",
+            ]
+        )
+    )
+    launch_description.add_action(moveit_robot_launch)
+
+    # Controller
+    # ! Errors here
+
+    """jaco_controller_node = Node(
+        package="my_jaco_controller",
+        executable="all_purpose_controller.py",
+        name="jaco_controller",
+        #parameters=["/config/params.yaml"],
+    )
+    launch_description.add_action(jaco_controller_node)"""
+
+    return launch_description
--- my_jaco_controller/build/my_jaco_controller/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/root/ws/my_jaco_controller/install/my_jaco_controller'
